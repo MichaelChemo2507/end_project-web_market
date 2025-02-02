5d60375010b27d909817bf76ef2333964cea4451
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-02-03 00:03:02 +0200

    create the header section with the relevant Links.

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -1,3 +1,15 @@
+import { Link, Outlet } from "react-router-dom";
+import Header from "./header";
+
 export default function MainPage() {
-    return(<></>)
+    return (<>
+        <Header>
+            <Link to='/admin' className="navBarLinks"> כניסת מנהל </Link>
+            <Link to='/store' className="navBarLinks"> דף הבית </Link>
+            <Link to='/shopingCart' className="navBarLinks"> סל קניות </Link>
+        </Header>   
+        <div className="mainSection">
+            <Outlet></Outlet>
+        </div>
+    </>)
 }
