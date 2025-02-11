812c10666a3254e4e926d8d800629ce6246b5f08
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-02-11 19:07:22 +0200

    Fixing some minor errors.

--- src/components/routs.jsx
+++ src/components/routs.jsx
@@ -89,6 +89,7 @@ export default function Routs() {
           action: async ({ request }) => {
             let obj = Object.fromEntries(await request.formData());
             let objProductsCart = JSON.parse(obj.productsCart);
+            
             if (obj) {
               let productsCode = objProductsCart.map((product) => {
                 return product.productCode;
